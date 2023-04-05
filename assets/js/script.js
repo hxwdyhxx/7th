10b5571b734861666660d8e03e5ed4160d1b7ce9
hxxppy <abcd_9595@daum.net>
2023-04-05 19:48:52 +0900

    7th

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,7 +94,6 @@ $(function(){
 
     
 
-
     // each
     const introduce = gsap.timeline({
         scrollTrigger:{
@@ -104,19 +103,52 @@ $(function(){
             // markers:true,
         },
     })
-
+    
+    const introText = new SplitType('.sc-introduce h2', { types: 'words, chars', });
+    introduce.addLabel('a')
     introduce
     .from('.sc-introduce .animate-pulse',{
         yPercent:10,
         opacity:0,
         stagger:0.1
-    })
+    },'a')
     .from('.sc-introduce .plus',{
         opacity:0,
+    },'a')
+    // title 
+    const introScrollText = gsap.from(introText.chars,{
+        scrollTrigger:{
+            trigger:".sc-introduce",
+            start:"0% 30%",
+            end:"50% 50%",
+            // markers:true,
+        },
+        yPercent:20,
+        paused:true,
+        opacity:0,
+        stagger:{
+            from: "random",
+            amount: 0.3,   
+        },
+    },'a')
+
+
+    const infoText = new SplitType('.sc-information .sc-title h3', { types: 'words, chars', });
+    const infoScrollText = gsap.from(infoText.chars,{
+        scrollTrigger:{
+            trigger:".sc-information",
+            start:"0% 30%",
+            end:"50% 50%",
+            // markers:true,
+        },
+        yPercent:20,
+        paused:true,
+        opacity:0,
+        stagger:{
+            from: "random",
+            amount: 0.3,   
+        },
     })
-
-
-   
     $('.sc-info .contents-area').each(function (index, elemnet) {
         gsap.from($(this).find('.text-wrap'), 1, {
             scrollTrigger: {
@@ -143,6 +175,60 @@ $(function(){
         });
         
     });
+
+    const adText = new SplitType('.sc-advertisement .animation-box h3', { types: 'words, chars', });
+
+    introduce.addLabel('b')
+    $('.sc-switch .sc-advertisement').each(function (index, elemnet) {
+        gsap.from($(this).find(adText.chars), {
+            scrollTrigger:{
+                        trigger:$(this),
+                        start:"0% 30%",
+                        end:"50% 50%",
+                        // markers:true,
+                    },
+                    yPercent:20,
+                    paused:true,
+                    opacity:0,
+                    stagger:{
+                        from: "random",
+                        amount: 0.3,   
+                    },
+        })
+        gsap.from($(this).find('.animation-box .ad-list li'),{
+            scrollTrigger:{
+                trigger:$(this),
+                start:"0% 30%",
+                end:"50% 50%",
+                // markers:true,
+            },
+            yPercent:20,
+            paused:true,
+            opacity:0,
+        })
+        gsap.from($(this).find('.animation-box .desc'),{
+            scrollTrigger:{
+                trigger:$(this),
+                start:"0% 30%",
+                end:"50% 50%",
+                // markers:true,
+            },
+            yPercent:20,
+            paused:true,
+            opacity:0,
+        })
+        gsap.from($(this).find('.image-wrap'),{
+            scrollTrigger:{
+                trigger:$(this),
+                start:"0% 30%",
+                end:"50% 50%",
+                // markers:true,
+            },
+            paused:true,
+            opacity:0,
+        })
+    });
+
 
 
     // loop slide/////////////////////
